4253603fa40b8a2bfc629aca45da74b14fbf9b6f
Babosa <mysunpany@gmail.com>
2015-08-04 13:28:44 +0800

    Add Copyright (C) EasyDarwin.org

--- Include/EasyPusherAPI.h
+++ Include/EasyPusherAPI.h
@@ -1,3 +1,9 @@
+/*
+	Copyright (c) 2013-2015 EasyDarwin.ORG.  All rights reserved.
+	Github: https://github.com/EasyDarwin
+	WEChat: EasyDarwin
+	Website: http://www.easydarwin.org
+*/
 #ifndef __EasyPusher_H__
 #define __EasyPusher_H__
 
