8c0f4a611c0dfb3b1b73b080b48895b6af1cab3b
whl1208@126.com <whl1208@126.com>
2016-04-23 17:34:12 +0800

    修改默认ID

--- EasyPusher_Android/app/src/main/java/org/easydarwin/config/Config.java
+++ EasyPusher_Android/app/src/main/java/org/easydarwin/config/Config.java
@@ -19,7 +19,7 @@ public class Config {
     public static final String STREAM_ID="streamId";
     public static final String DEFAULT_SERVER_IP="121.40.50.44";
     public static final String DEFAULT_SERVER_PORT="554";
-    public static final String DEFAULT_STREAM_ID= String.valueOf(System.currentTimeMillis());
+    public static final String DEFAULT_STREAM_ID= String.valueOf((int)(Math.random()*1000000+100000));
     public static final String PREF_NAME="easy_pref";
     public static final String K_RESOLUTION="k_resolution";
 
